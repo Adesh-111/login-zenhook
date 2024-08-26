dafbc1154c31c4991ac700d4ba1ec37ff0ae7e83
Adesh-111 <adeshabinesh25@gmail.com>
2024-08-26 20:24:53 +0530

    Implement of first version with some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,118 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+
 function App() {
+  // app.render()
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/login', { email, password });
+      if (response.data.success) {
+        alert('User added');
+      } else {
+        alert('Failed to add user');
+      }
+    } catch (error) {
+      console.error('There was an error adding the user!', error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(password);
+  }, [email, password]);
+
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePassword(event) {
+    setPassword(event.target.value);
+  }
 
   return (
     <>
       <div className="limiter">
-		<div className="container-login100">
-			<div className="wrap-login100">
-				<div className="login100-pic js-tilt" data-tilt>
-					<img src="src\images\img-01.png" alt="IMG" />
-				</div>
+        <div className="container-login100">
+          <div className="wrap-login100">
+            <div className="login100-pic js-tilt" data-tilt>
+              <img src="src\images\img-01.png" alt="IMG" />
+            </div>
 
-				<form className="login100-form validate-form">
-					<span className="login100-form-title">
-						Member Login
-					</span>
+            <form className="login100-form validate-form" action="post" onSubmit={handleSubmit}>
+              <span className="login100-form-title">Member Login</span>
 
-					<div className="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input className="input100" type="text" name="email" placeholder="Email" />
-						<span className="focus-input100"></span>
-						<span className="symbol-input100">
-							<i className="fa fa-envelope" aria-hidden="true"></i>
-						</span>
-					</div>
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Valid email is required: ex@abc.xyz"
+              >
+                <input
+                  className="input100"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  onChange={handleEmail}
+                  
+                />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i className="fa fa-envelope" aria-hidden="true"></i>
+                </span>
+              </div>
 
-					<div className="wrap-input100 validate-input" data-validate = "Password is required">
-						<input className="input100" type="password" name="pass" placeholder="Password" />
-						<span className="focus-input100"></span>
-						<span className="symbol-input100">
-							<i className="fa fa-lock" aria-hidden="true"></i>
-						</span>
-					</div>
-					
-					<div className="container-login100-form-btn">
-						<button className="login100-form-btn">
-							Login
-						</button>
-					</div>
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Password is required"
+              >
+                <input
+                  className="input100"
+                  type="password"
+                  name="pass"
+                  placeholder="Password"
+                  onChange={handlePassword}
+                />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i className="fa fa-lock" aria-hidden="true"></i>
+                </span>
+              </div>
 
-					<div className="text-center p-t-12">
-						<span className="txt1">
-							Forgot
-						</span>
-						<a className="txt2" href="#">
-							Username / Password?
-						</a>
-					</div>
+              <div className="container-login100-form-btn">
+                <button className="login100-form-btn">
+                  <input
+                    type="submit"
+                    style={{ display: "none", padding: 0, margin: 0 }}
+                  />
+                  Login
+                </button>
+              </div>
 
-					<div className="text-center p-t-136">
-						<a className="txt2" href="#">
-							Create your Account
-							<i className="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-						</a>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	
+              <div className="text-center p-t-12">
+                <span className="txt1">Forgot</span>
+                <a className="txt2" href="#">
+                  Username / Password?
+                </a>
+              </div>
+
+              <div className="text-center p-t-136">
+                <a className="txt2" href="#">
+                  Create your Account
+                  <i
+                    className="fa fa-long-arrow-right m-l-5"
+                    aria-hidden="true"
+                  ></i>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+import env from "dotenv";
+import cors from "cors";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+env.config();
+const port = 3000;
+app.use(cors()); 
+
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+// Should print 'string'
+
+
+db.connect();
+
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  console.log(email);
+  
+  try {
+    const result = await db.query(
+      "INSERT INTO login (email, password) VALUES ($1, $2)",
+      [email, password]
+    );
+    res.status(201).json({ success: true, message: "User added successfully" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error inserting data" });
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
